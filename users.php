413a215a2b9787f673d6fe200da772cd4d244044
Ernestas-wq <54524989+Ernestas-wq@users.noreply.github.com>
2021-01-06 12:30:04 +0200

    Removed unnecessary users

--- users.php
+++ users.php
@@ -17,10 +17,7 @@ $password = "mysql";
 try {
     $conn = new PDO("mysql:host=$servername", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $SQL = "CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'adm';
-    GRANT CREATE, DELETE, DROP, INSERT, SELECT, REFERENCES, UPDATE, GRANT OPTION ON projectmanagerdb.*
-    TO 'admin'@'localhost';
-    CREATE USER IF NOT EXISTS 'app_user'@'localhost' IDENTIFIED BY 'app';
+    $SQL = "CREATE USER IF NOT EXISTS 'app_user'@'localhost' IDENTIFIED BY 'app';
     GRANT DELETE, INSERT, SELECT, UPDATE ON projectmanagerdb.*
     TO 'app_user'@'localhost';
     CREATE USER IF NOT EXISTS 'app_viewer'@'localhost' IDENTIFIED BY 'viewer';
